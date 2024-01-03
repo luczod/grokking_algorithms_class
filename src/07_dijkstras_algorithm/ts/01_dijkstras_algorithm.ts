d841d379c6ff82d41b65e04620aad2acbcf5004d
luczod <kzod5611@gmail.com>
2024-01-03 07:31:11 -0300

    cap07 : dijkstras algorithm

--- src/07_dijkstras_algorithm/ts/01_dijkstras_algorithm.ts
+++ src/07_dijkstras_algorithm/ts/01_dijkstras_algorithm.ts
@@ -12,8 +12,8 @@ graphWeight['a'] = {};
 graphWeight['a']['fin'] = 1;
 graphWeight['b'] = {};
 graphWeight['b']['a'] = 3;
-graphWeight['b']['end'] = 5;
-graphWeight['end'] = {};
+graphWeight['b']['fin'] = 5;
+graphWeight['fin'] = {};
 
 console.log(graphWeight);
 
@@ -32,14 +32,14 @@ parents['fin'] = null;
 // Avoid being processed more than once:
 const processed: string[] = [];
 
-function findLowestCostNode(costs: TCoast): string | null {
+function findLowestCostNode(costs: TCoast): string {
   let lowestCost = Infinity;
-  let lowestCostNode: string | null = null;
+  let lowestCostNode: string = '';
   // Go through each node.
   for (const node in costs) {
     const cost = costs[node];
     // If it's the lowest cost so far and hasn't been processed yet
-    if (cost < lowestCost && !Boolean(node in processed)) {
+    if (cost < lowestCost && !processed.includes(node)) {
       // ... set it as the new lowest-cost node.
       lowestCost = cost;
       lowestCostNode = node;
@@ -48,22 +48,16 @@ function findLowestCostNode(costs: TCoast): string | null {
 
   return lowestCostNode;
 }
-
-function dijkstras() {
+let node = findLowestCostNode(costs);
+export function dijkstras() {
   // Find the lowest-cost node that you haven't processed yet.
-  let node = findLowestCostNode(costs);
-
-  while (processed.length < 4) {
+  while (node !== '') {
     const cost = costs[node];
-    console.log(cost);
-    console.log(node);
+    console.log(node, cost);
     // Go through all the neighbors of this node.
     const neighbors = graphWeight[node];
     for (const n of Object.keys(neighbors)) {
       const newCost = cost + neighbors[n];
-      console.log(costs[n]);
-      console.log(newCost);
-
       // If it's cheaper to get to this neighbor by going through this node
       if (costs[n] > newCost) {
         // update the cost for this node.
@@ -78,7 +72,6 @@ function dijkstras() {
     node = findLowestCostNode(costs);
   }
 }
-
 dijkstras();
 console.log('Cost from the start to each node:');
-// console.table(costs);
+console.table(costs);
