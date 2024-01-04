ab8cfd1a68553491f3fce8866726117c9db2363c
luczod <kzod5611@gmail.com>
2024-01-04 12:23:14 -0300

    common subsequence improve

--- src/09_dynamic_programming/ts/02_common_subsequence.ts
+++ src/09_dynamic_programming/ts/02_common_subsequence.ts
@@ -4,16 +4,16 @@ import { dpTable, tableBlue, tableClues } from './01_common_substring';
 console.table(dpTable);
 // i -> row
 // j -> column
-for (let i = 0; i < tableBlue.length - 1; i++) {
-  for (let j = 0; j < tableClues.length - 1; j++) {
+for (let i = 0; i < tableBlue.length; i++) {
+  for (let j = 0; j < tableClues.length; j++) {
     if (tableClues[j] == tableBlue[i]) {
       // if the letters are the same,
       // the value is equal to the value of the upper left neighbor + 1
-      dpTable[i][j] = dpTable[i - 1][i - 1] + 1;
+      dpTable[j][i] = dpTable[j - 1][i - 1] + 1;
     } else {
       // if the letters are not the same
       // choose the biggest one among them
-      dpTable[i][j] = Math.max(dpTable[j - 1][i], dpTable[j][i - 1]);
+      dpTable[j][i] = Math.max(j > 0 ? dpTable[j - 1][i] : 0, i > 0 ? dpTable[j][i - 1] : 0);
     }
   }
 }
